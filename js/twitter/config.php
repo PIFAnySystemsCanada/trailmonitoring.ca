9eaa01ea7b30983ecc95bafc7a8fa81c2ae22cca
Mark Buckaway <mark@buckaway.ca>
2019-04-19 22:05:34 -0400

    initial commit with updated website

---
+++ js/twitter/config.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+	'consumerKey' => 'yPHh4hPLkjWdE3iWhL5ANH30J',
+	'consumerSecret' => 'rL9HEb9CGln5F2rJvZon27GNUcHanUWiejVDRw39IjCQVZURO0',
+	'accessToken' => '1088797497642885120-g6swg0Xp6wWHlbV0qcoeZBpJjgbKib',
+	'accessTokenSecret' => 'kv3xbYFABymqCCzpN46VqHpWSaVQaXh3SbLPtcGMq0Srs',
+);
