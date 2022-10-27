9ba379fd87022c743ff801654808b275012603ea
Mark Buckaway <mark@buckaway.ca>
2022-10-26 22:38:26 -0400

    Refactored to support getting data from timestream API and remove all references from grafana

--- js/utility.js
+++ js/utility.js
@@ -1,24 +1,93 @@
-
 /**
- * @file Bunch of utilities to handle getting data from ThingsSpeak channels
+ * @file Bunch of utilities to handle getting data from Timestream Data API
  * @author Mark Buckaway <mark@buckaway.ca>
- * @version 0.1
+ * @version 0.2
  */
 
 /**
-  * List of thingsspeak urls for out channels
+ * Sensor ID's
+ */
+var snyders_weather = "synders_0766BC";
+var glasgow_weather = "glasgow_e969EC";
+var snyders_solar = "snyders_5e6870";
+var glasgow_weather = "glasgow_53F0E0";
+
+var api_url = "https://api.trailrabbit.io/timestream"
+var measures = {
+    "groundtemperature": "weather",
+    "temperature": "weather",
+    "lightlevel": "weather",
+    "pressure": "weather",
+    "current_acc_rain": "weather",
+    "event_acc_rain": "weather",
+    "mm_per_hour_rain": "weather",
+    "humidity": "weather",
+    "total_rain": "weather",
+    "groundmoisture": "weather",
+    "dewpoint": "weather",
+    "load_power": "solar",
+    "battery_current": "solar",
+    "array_power": "solar",
+    "array_current": "solar",
+    "battery_soc": "solar",
+    "array_voltage": "solar",
+    "battery_power": "solar",
+    "load_voltage": "solar",
+    "load_current": "solar",
+    "battery_temp": "solar",
+    "battery_voltage": "solar"    
+}
+
+function getMainUrl(type, location) {
+    if (type == "weather") {
+        if (location == "snyders") {
+            sensorid = snyders_weather
+        } else {
+            sensorid = glasgow_weather
+        }
+    } else {
+        if (location == "snyders") {
+            sensorid = snyders_solar
+        } else {
+            sensorid = glasgow_solar
+        }
+    }
+    return `${api_url}/${type}/measures/${sensorid}?timerange=6h&order=ASC&timezone=America%2FToronto`
+}
+/**
+  * Gets the Timestream Data URL 
+  * @param location Location snyders or glasgow
+  * @param measure Name of the measure (temperature, etc)
+  * @param len Type of measure: last or series
   */
-var thingspeakurls =
-    {
-        "tempdata": "https://api.thingspeak.com/channels/738696/feeds.json",
-        "solardata": "https://api.thingspeak.com/channels/1228246/feeds.json",
-        "espdata": "https://api.thingspeak.com/channels/1191345/feeds.json",
-        "espage": "https://api.thingspeak.com/channels/1191345/feeds/last_data_age.json",
-        "solarage": "https://api.thingspeak.com/channels/1228246/feeds/last_data_age.json",
-        "tempage": "https://api.thingspeak.com/channels/738696/feeds/last_data_age.json",
-    };
-
-
+function getUrl(location, measure, len) 
+{
+    var sensorid = ""
+    var type = measures[measure]
+    if (type == undefined) {
+        type = "weather"
+    }
+    if (type == "weather") {
+        if (location == "snyders") {
+            sensorid = snyders_weather;
+        } else {
+            sensorid = glasgow_weather;
+        }
+    } else {
+        if (location == "snyders") {
+            sensorid = snyders_solar;
+        } else {
+            sensorid = glasgow_solar;
+        }
+    }
+    var url = "";
+    if (len == "last") {
+        url = `${api_url}/${type}/sensor/${sensorid}/${measure}/last?timezone=America%2FToronto`
+    } else {
+        url = `${api_url}/${type}/sensor/${sensorid}/${measure}/series?timerange=6h&order=ASC&timezone=America%2FToronto`
+    }
+    return url
+}
 /**
   * Gets the current date as a UTC date string (only the date, the time is 00:00:00)
   */
@@ -31,39 +100,6 @@ function getDateString()
     return dateString;
 }
 
-/**
-  * Gets the Thingspeak URL with the date filter
-  * @param urlname Name of the URL from the thinkspeakurls hash
-  */
-function getThingsSpeakURLByDate(urlname)
-{
-    var dateString = getDateString();
-//    var result = thingspeakurls[urlname] + "?days=1&start=" + dateString;
-    var result = thingspeakurls[urlname] + "?days=1&timezone=America%2FNew_York";
-    //var result = thingspeakurls[urlname] + "?count=1440";
-    return result;
-}
-
-/**
-  * Gets the Thingspeak URL with the date filter
-  * @param urlname Name of the URL from the thinkspeakurls hash
-  */
- function getThingsSpeakURLLastItem(urlname)
- {
-     var result = thingspeakurls[urlname] + "?results=1";
-     return result;
- }
-
-/**
-  * Gets the Thingspeak URL without any filter
-  * @param urlname Name of the URL from the thinkspeakurls hash
-  */
- function getThingsSpeakURL(urlname)
- {
-     var result = thingspeakurls[urlname];
-     return result;
- }
-
  /**
   * Gets the json Data from a REST call. This generic and works anywhere.
   * This uses the HTTP GET to retrieve data.
@@ -75,11 +111,15 @@ function getThingsSpeakURLByDate(urlname)
   */
 
 async function getRESTData(url) {
+    console.log(`getRESTData: Fetching url: ${url}`)
     try {
         return await ( 
             await fetch(url,
                 {
-                    headers: { "Content-Type": "application/json" },
+                    headers: { 
+                        "Content-Type": "application/json",
+                        'Authorization': 'Bearer EYgS8oLU0c7ejT0C9Dhlv6KsF7ePpJad8m1WltxO'
+                    },
                     method: "GET"
                 }
             )
--- js/weatherdata.js
+++ js/weatherdata.js
@@ -4,7 +4,10 @@ class WeatherWidgets
 	{
 		this.dialDiv = 'dials';
 		this.lightChartDiv = 'lightChart';
+		this.rainChartDiv = 'rainChart'
 		this.temperatureChartDiv = 'temperatureChart';
+		this.humidityChartDiv = 'humidityChart';
+		this.airPressureChartDiv = 'airPressureChart';
 		// Create the dials for the current data display
 		this.dialData = [
 			{
@@ -14,19 +17,28 @@ class WeatherWidgets
 				type: "indicator",
 				mode: "number+delta+gauge",
 				delta: { reference: 0 },
-				gauge: { axis: { range: [-30, 30] } }
+				gauge: { axis: { range: [-30, 40] } }
 			},
 			{
 				domain: { row: 0, column: 1 },
+				value: 0,
+				title: { text: "Current Humidity (%)" },
+				type: "indicator",
+				mode: "number+delta+gauge",
+				delta: { reference: 0 },
+				gauge: { axis: { range: [0, 100] } }
+			  },
+			{
+				domain: { row: 0, column: 2 },
 				value: 0,
 				title: { text: "Current Ground Temp (C)" },
 				type: "indicator",
 				mode: "number+delta+gauge",
 				delta: { reference: 0 },
-				gauge: { axis: { range: [-30, 30] } }
+				gauge: { axis: { range: [-30, 40] } }
 			  },
 			  {
-				domain: { row: 0, column: 2 },
+				domain: { row: 0, column: 3 },
 				value: 0,
 				title: { text: "Current Light Level (lux)" },
 				type: "indicator",
@@ -34,13 +46,76 @@ class WeatherWidgets
 				delta: { reference: 0 },
 				gauge: { axis: { range: [0, 5000] } }
 			  },
+			  {
+				domain: { row: 1, column: 0 },
+				value: 0,
+				title: { text: "Humidex (C)" },
+				type: "indicator",
+				mode: "number+gauge",
+				delta: { reference: 0 },
+				gauge: { axis: { range: [-30, 50] } }
+			  },
+			  {
+				domain: { row: 1, column: 1 },
+				value: 0,
+				title: { text: "Rain per Hour (mm)" },
+				type: "indicator",
+				mode: "number+delta+gauge",
+				delta: { reference: 0 },
+				gauge: { axis: { range: [0, 100] } }
+			  },
+			  {
+				domain: { row: 1, column: 2 },
+				value: 0,
+				title: { text: "Ground Moisture" },
+				type: "indicator",
+				mode: "number+delta+gauge",
+				delta: { reference: 0 },
+				gauge: { axis: { range: [0, 2500] } }
+			  },
+			  {
+				domain: { row: 1, column: 3 },
+				value: 0.0,
+				title: { text: "Air Pressure (hpa)" },
+				type: "indicator",
+				mode: "number+delta+gauge",
+				delta: { reference: 0 },
+				gauge: { axis: { range: [900, 1200] } }
+			  },
+			  {
+				domain: { row: 2, column: 0 },
+				value: 0,
+				title: { text: "Event Rainfall (mm)" },
+				type: "indicator",
+				mode: "number+gauge",
+				delta: { reference: 0 },
+				gauge: { axis: { range: [0, 100] } }
+			  },
+			  {
+				domain: { row: 2, column: 1 },
+				value: 0,
+				title: { text: "Period Rainfall (mm)" },
+				type: "indicator",
+				mode: "number+gauge",
+				delta: { reference: 0 },
+				gauge: { axis: { range: [0, 100] } }
+			  },
+			  {
+				domain: { row: 2, column: 2 },
+				value: 0,
+				title: { text: "Dewpoint (C)" },
+				type: "indicator",
+				mode: "number+gauge",
+				delta: { reference: 0 },
+				gauge: { axis: { range: [-10, 40] } }
+			  },
 			];
 
 		this.dialLayout = {
-			width: 900,
-			height: 300,
-			margin: { t: 10, b: 10, l: 50, r: 50 },
-			grid: { rows: 1, columns: 3, pattern: "independent" },
+			width: 1200,
+			height: 750,
+			margin: { t: 10, b: 10, l: 40, r: 50 },
+			grid: { rows: 3, columns: 4, pattern: "independent" },
 			font: { color: "#ffffff" },
 			plot_bgcolor: 'rgba(0,0,0,0)',
 			paper_bgcolor: 'rgba(0,0,0,0)',
@@ -75,16 +150,88 @@ class WeatherWidgets
 				line: {color: '#08FF08'}
 			}
 		];
-		this.chartLayout = {
-			width: 900,
+		this.rainChartLayout = {
+			width: 1200,
 			height: 400,
-			title: 'Temperature Last 24 hrs (C)',
+			title: 'Rain 6hrs (mm)',
+			font: { color: "#ffffff" },
+			plot_bgcolor: 'rgba(0,0,0,0)',
+			paper_bgcolor: 'rgba(0,0,0,0)',
+			yaxis: {
+				autorange: false,
+				range: [0, 40],
+				type: 'linear'
+			}
+		};
+
+		this.rainChartData = [
+			{
+				x: [dateString],
+				y: [0],
+				type: 'scatter',
+				mode: "lines",
+				name: 'Rain (mm)',
+				line: {color: '#FFE900'}
+			}
+		];
+		this.chartLayout = {
+			width: 1200,
+			height: 400,
+			title: 'Temperature Last 6hrs (C)',
 			font: { color: "#ffffff" },
 			plot_bgcolor: 'rgba(0,0,0,0)',
 			paper_bgcolor: 'rgba(0,0,0,0)',
 			yaxis: {
 				autorange: false,
 				range: [-30, 40],
+				type: 'linear'
+			}
+		};
+
+		this.humidityChartData = [
+			{
+				x: [dateString],
+				y: [0],
+				type: 'scatter',
+				mode: "lines",
+				name: 'Humidity (%)',
+				line: {color: '#17BECF'},
+				}
+		];
+		this.humidityChartLayout = {
+			width: 1200,
+			height: 400,
+			font: { color: "#ffffff" },
+			plot_bgcolor: 'rgba(0,0,0,0)',
+			paper_bgcolor: 'rgba(0,0,0,0)',
+			title: 'Humidity Last 6hrs (%)',
+			yaxis: {
+				autorange: false,
+				range: [0, 100],
+				type: 'linear'
+			}
+		};
+
+		this.airPressureChartData = [
+			{
+				x: [dateString],
+				y: [0],
+				type: 'scatter',
+				mode: "lines",
+				name: 'Air Pressure (hpa)',
+				line: {color: '#17BECF'},
+				}
+		];
+		this.airPressureChartLayout = {
+			width: 1200,
+			height: 400,
+			font: { color: "#ffffff" },
+			plot_bgcolor: 'rgba(0,0,0,0)',
+			paper_bgcolor: 'rgba(0,0,0,0)',
+			title: 'Air Pressure Last 6hrs (hpa)',
+			yaxis: {
+				autorange: false,
+				range: [900, 1200],
 				type: 'linear'
 			}
 		};
@@ -100,12 +247,12 @@ class WeatherWidgets
 				}
 		];
 		this.lightchartLayout = {
-			width: 900,
+			width: 1200,
 			height: 400,
 			font: { color: "#ffffff" },
 			plot_bgcolor: 'rgba(0,0,0,0)',
 			paper_bgcolor: 'rgba(0,0,0,0)',
-			title: 'Light Level Last 24 hrs (lux)',
+			title: 'Light Level Last 6hrs (lux)',
 			yaxis: {
 				autorange: false,
 				range: [0, 6000],
@@ -120,18 +267,68 @@ class WeatherWidgets
 		this.dialData[0].delta.reference = reference;
 	}
 
-	setGroundTempDials(value, reference)
+	setHumidityDials(value, reference)
 	{
 		this.dialData[1].value = value;
 		this.dialData[1].delta.reference = reference;
 	}
 
-	setLightLevelDials(value, reference)
+	setGroundTempDials(value, reference)
 	{
 		this.dialData[2].value = value;
-		this.dialData[2].delta.reference = reference;
+		this.dialData[3].delta.reference = reference;
 	}
 
+	setLightLevelDials(value, reference)
+	{
+		this.dialData[3].value = value;
+		this.dialData[3].delta.reference = reference;
+	}
+
+	setHumidexDials(value)
+	{
+		this.dialData[4].value = value;
+	}
+
+	setRainPerHourDials(value, reference)
+	{
+		this.dialData[5].value = value;
+		this.dialData[5].delta.reference = reference;
+	}
+
+	setGroundMoistureDials(value, reference)
+	{
+		this.dialData[6].value = value;
+		this.dialData[6].delta.reference = reference;
+	}
+
+	setAirPressureDials(value, reference)
+	{
+		this.dialData[7].value = value;
+		this.dialData[7].delta.reference = reference;
+	}
+
+	setEventRainfallDials(value)
+	{
+		this.dialData[8].value = value;
+	}
+
+	setPeriodRainfallDials(value)
+	{
+		this.dialData[9].value = value;
+	}
+
+	setDewpointDials(value)
+	{
+		this.dialData[10].value = value;
+	}
+
+	setRainPerHourChart(timedata, ydata)
+	{
+		this.rainChartData[0].x = timedata;
+		this.rainChartData[0].y = ydata;
+	}
+	
 	setAirTempChart(timedata, ydata)
 	{
 		this.chartData[0].x = timedata;
@@ -144,6 +341,18 @@ class WeatherWidgets
 		this.chartData[1].y = ydata;
 	}
 
+	setHumidityChart(timedata, ydata)
+	{
+		this.humidityChartData[0].x = timedata;
+		this.humidityChartData[0].y = ydata;
+	}
+
+	setAirPressureChart(timedata, ydata)
+	{
+		this.airPressureChartData[0].x = timedata;
+		this.airPressureChartData[0].y = ydata;
+	}
+
 	setLightLevelChart(timedata, ydata)
 	{
 		this.lightchartData[0].x = timedata;
@@ -153,16 +362,21 @@ class WeatherWidgets
 	drawWidgets()
 	{
 		Plotly.react(this.dialDiv,this.dialData, this.dialLayout, { responsive: true });
-		Plotly.react(this.temperatureChartDiv, this.chartData, this.chartLayout);
-		Plotly.react(this.lightChartDiv, this.lightchartData, this.lightchartLayout);		  			  
+		Plotly.react(this.temperatureChartDiv, this.chartData, this.chartLayout, { responsive: true });
+		Plotly.react(this.humidityChartDiv, this.humidityChartData, this.humidityChartLayout, { responsive: true });
+		Plotly.react(this.airPressureChartDiv, this.airPressureChartData, this.airPressureChartLayout, { responsive: true });
+		Plotly.react(this.rainChartDiv, this.rainChartData, this.rainChartLayout, { responsive: true });		  			  
+		Plotly.react(this.lightChartDiv, this.lightchartData, this.lightchartLayout, { responsive: true });		  			  
 	}
 
 }
 
 var alreadyInitialized = false;
+//var measuresdata = getRESTData(getMainUrl("weather", "snyders"))
 
-function updateSolarWidgets()
+function updateWeatherWidgets()
 {
+		var lasttime = new Date()
 		var weatherWidgets = new WeatherWidgets();
 		if (!alreadyInitialized)
 		{
@@ -176,36 +390,17 @@ function updateSolarWidgets()
 
 		// Download and clean up our data
 		(async function () {
-			var trailcamdata = await getRESTData(getThingsSpeakURLByDate("tempdata"));
+			var lightdata = await getRESTData(getUrl("snyders", "lightlevel", "series"));
 			var timedata_x = new Array();
-			var airtempdata_y = new Array();
 			var lightdata_y = new Array();
-			var maxtemp = 0.0;
-			var currenttemp = 0.0;
-			var maxlight = 0;
-			var currentlight = 0;
-			trailcamdata.feeds.forEach(item => {
-				var temp = parseFloat(item['field3'])
-				var light = parseFloat(item['field4'])
-				timedata_x.push(item['created_at']);
-				airtempdata_y.push(temp);
-				lightdata_y.push(light);
-				if (temp>maxtemp)
-				{
-					maxtemp = temp;
-				}
-				if (light>maxlight)
-				{
-					maxlight = light;
-				}
-
+			var currentlight = lightdata["items"]["last"]["value"];
+			lightdata.items.data.forEach( item => {
+				var temp = parseFloat(item['value']);
+			 	timedata_x.push(item['time']);
+			 	lightdata_y.push(temp);
 			});
-			currenttemp = airtempdata_y[airtempdata_y.length-1];
-			currentlight = lightdata_y[lightdata_y.length-1];
 
-			weatherWidgets.setAirTempDials(currenttemp, maxtemp);
-			weatherWidgets.setLightLevelDials(currentlight, maxlight);
-			weatherWidgets.setAirTempChart(timedata_x, airtempdata_y);
+			weatherWidgets.setLightLevelDials(currentlight, 5500);
 			weatherWidgets.setLightLevelChart(timedata_x, lightdata_y);
 			weatherWidgets.drawWidgets();
 			loading.hidden = true;
@@ -213,37 +408,156 @@ function updateSolarWidgets()
 		})();
 
 		(async function () {
-			var sensordata = await getRESTData(getThingsSpeakURLByDate("espdata"));
+			var raindata = await getRESTData(getUrl("snyders", "mm_per_hour_rain", "series"));
+			var timedata_x = new Array();
+			var raindata_y = new Array();
+			var currentrain = raindata["items"]["last"]["value"];
+			var maxrain = raindata["items"]["max"]["value"];
+			raindata.items.data.forEach( item => {
+				var temp = parseFloat(item['value']);
+			 	timedata_x.push(item['time']);
+			 	raindata_y.push(temp);
+			});
+
+			weatherWidgets.setRainPerHourDials(currentrain, maxrain);
+			weatherWidgets.setRainPerHourChart(timedata_x, raindata_y);
+			weatherWidgets.drawWidgets();
+			loading.hidden = true;
+			loaded.hidden = false;
+		})();
+
+		(async function () {
+			var temperaturedata = await getRESTData(getUrl("snyders", "temperature", "series"));
+			var timedata_x = new Array();
+			var airtempdata_y = new Array();
+			var maxtemp = temperaturedata.items.max.value;
+			var currenttemp = temperaturedata["items"]["last"]["value"];
+			lasttime = temperaturedata["items"]["last"]["time"];
+			temperaturedata.items.data.forEach( item => {
+				var temp = parseFloat(item['value']);
+			 	timedata_x.push(item['time']);
+			 	airtempdata_y.push(temp);
+			});
+
+			weatherWidgets.setAirTempDials(currenttemp, maxtemp);
+			weatherWidgets.setAirTempChart(timedata_x, airtempdata_y);
+			weatherWidgets.drawWidgets();
+			loading.hidden = true;
+			loaded.hidden = false;
+		})();
+
+		(async function () {
+			var humiditydata = await getRESTData(getUrl("snyders", "humidity", "series"));
+			var timedata_x = new Array();
+			var humiditydata_y = new Array();
+			var maxhumidiy = humiditydata.items.max.value;
+			var currenthumidity = humiditydata["items"]["last"]["value"];
+			humiditydata.items.data.forEach( item => {
+				var temp = parseFloat(item['value']);
+			 	timedata_x.push(item['time']);
+			 	humiditydata_y.push(temp);
+			});
+
+			weatherWidgets.setHumidityDials(currenthumidity, maxhumidiy);
+			weatherWidgets.setHumidityChart(timedata_x, humiditydata_y);
+			weatherWidgets.drawWidgets();
+			loading.hidden = true;
+			loaded.hidden = false;
+		})();
+
+		(async function () {
+			var airpressuredata = await getRESTData(getUrl("snyders", "pressure", "series"));
+			var timedata_x = new Array();
+			var airpressuredata_y = new Array();
+			var maxairpressure = airpressuredata.items.max.value;
+			var currentairpressure = airpressuredata["items"]["last"]["value"];
+			airpressuredata.items.data.forEach( item => {
+				var temp = parseFloat(item['value']);
+			 	timedata_x.push(item['time']);
+			 	airpressuredata_y.push(temp);
+			});
+
+			weatherWidgets.setAirPressureDials(currentairpressure, maxairpressure);
+			weatherWidgets.setAirPressureChart(timedata_x, airpressuredata_y);
+			weatherWidgets.drawWidgets();
+			loading.hidden = true;
+			loaded.hidden = false;
+		})();
+
+		(async function () {
+			var sensordata = await getRESTData(getUrl("snyders", "groundtemperature", "series"));
+			var ok = sensordata["properties"]["ok"]
+			if (ok != "1") {
+				console.log("unable to get ground temp data")
+			}
 			var timedata_x = new Array();
 			var groundtempdata_y = new Array();
-			var maxgroundtemp = 0.0;
-			var currentgroundtemp = 0.0;
-			sensordata.feeds.forEach(item => {
-				// Clean up field2 and field5 because nothing else is actually used
+			var maxgroundtemp = sensordata["items"]["max"]["value"];
+			var currentgroundtemp = sensordata["items"]["last"]["value"];
+			sensordata.items.data.forEach(item => {
 				var groundtemp = parseFloat(item['field2']);
-				var rainlevel = parseFloat(item['field5']);
-				timedata_x.push(item['created_at']);
+				timedata_x.push(item['time']);
 				groundtempdata_y.push(groundtemp);
-				if (groundtemp>maxgroundtemp)
-				{
-					maxgroundtemp = groundtemp;
-				}
 			});
-			currentgroundtemp = groundtempdata_y[groundtempdata_y.length-1];
-
 			weatherWidgets.setGroundTempDials(currentgroundtemp, maxgroundtemp);
 			weatherWidgets.setGroundTempChart(timedata_x, groundtempdata_y);
 			weatherWidgets.drawWidgets();
 		})();
 
+		(async function () {
+			var sensordata = await getRESTData(getUrl("snyders", "event_acc_rain", "last"));
+			var ok = sensordata["properties"]["ok"]
+			if (ok != "1") {
+				console.log("unable to get ground temp data");
+			}
+			var currenteventrain = sensordata["items"]["value"];
+			weatherWidgets.setEventRainfallDials(currenteventrain);
+			weatherWidgets.drawWidgets();
+		})();
+
+		(async function () {
+			var sensordata = await getRESTData(getUrl("snyders", "current_acc_rain", "last"));
+			var ok = sensordata["properties"]["ok"]
+			if (ok != "1") {
+				console.log("unable to get ground temp data");
+			}
+			var currenteventrain = sensordata["items"]["value"];
+			weatherWidgets.setPeriodRainfallDials(currenteventrain);
+			weatherWidgets.drawWidgets();
+		})();
+
+		(async function () {
+			var sensordata = await getRESTData(getUrl("snyders", "dewpoint", "last"));
+			var ok = sensordata["properties"]["ok"]
+			if (ok == "1") {
+				var currenteventrain = sensordata["items"]["value"];
+				weatherWidgets.setDewpointDials(currenteventrain);
+				weatherWidgets.drawWidgets();
+			} else {
+				console.log("unable to get dewpoint data");
+			}
+		})();
+
+		(async function () {
+			var sensordata = await getRESTData(getUrl("snyders", "humidex", "last"));
+			var ok = sensordata["properties"]["ok"]
+			if (ok == "1") {
+				var currentvalue = sensordata["items"]["value"];
+				weatherWidgets.setHumidexDials(currentvalue);
+				weatherWidgets.drawWidgets();
+			} else {
+				console.log("unable to get dewpoint data");
+			}
+		})();
+
 		(async function() {
-			var espage = await getRESTData(getThingsSpeakURL("espage"));
 			var loaded_text = document.getElementById("status_text");
-			if (espage.last_data_age>3600)
+			var age = Date.now() - lasttime;
+			if (age>3600000)
 			{
 				loaded_text.innerHTML="Loaded but Stale (offline?)";
 			}
-			else if (espage.last_data_age>600)
+			else if (age>600000)
 			{
 				loaded_text.innerHTML="Loaded but Old";
 			}
@@ -258,7 +572,7 @@ function updateSolarWidgets()
 ;(function($, window) {
 
 	$(document).ready(function() {
-		updateSolarWidgets();
-		window.setInterval(updateSolarWidgets, 60000);
+		updateWeatherWidgets();
+		window.setInterval(updateWeatherWidgets, 60000);
 	}); // End (document).ready
 })(jQuery, window);
